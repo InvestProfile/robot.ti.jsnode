c37f4267a5b3f6f684239a472ec1f89a26599bfb
mil <milomory@gmail.com>
2024-02-14 01:55:44 +0300

    exclude build

--- build/debug-instance.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const saas_globals_1 = require("saas-globals");
-exports.default = (0, saas_globals_1.setupDebug)({ name: 'robot.ti.jsnode', root: __filename });
