78d2948b706090d417a01459fb28e035052e66b6
mil <milomory@gmail.com>
2024-02-14 12:03:24 +0300

    min mods

--- app/index.ts
+++ app/index.ts
@@ -1,34 +1,33 @@
 
 
-// import initDebug from './debug-instance'
-// const debug = initDebug(__filename)
-// debug('hui')
-
-const greeting: string = "Hello, TypeScript!";
-console.log(greeting);
+import initDebug from './debug-instance'
+const debug = initDebug(__filename)
+debug('hui')
 
 
-import { getEnv } from './config/env.config';
 
-const envVariables = getEnv();
 
-console.log(`The Tinkoff INVEST_TOKEN is: ${envVariables.INVEST_TOKEN}`);
-
-import { createSdk } from 'tinkoff-sdk-grpc-js';
-
-!(async function example() {
-    if (envVariables.INVEST_TOKEN) {
-        const {users, operations} = createSdk(envVariables.INVEST_TOKEN);
-
-        const userInfo = await users.getInfo({});
-        const accounts = await users.getAccounts({});
-        const tarrif = await users.getUserTariff({});
-        console.log('Информация о пользователе:', userInfo);
-        console.log('Информация о счетах:', accounts);
-        console.log('Информация о тарифе пользователя:', tarrif);
-
-        const positions = await operations.getPortfolio({accountId: '2054310628'})
-        console.log("positions", positions)
-
-    }
-})();
+// import { getEnv } from './config/env.config';
+//
+// const envVariables = getEnv();
+//
+// console.log(`The Tinkoff INVEST_TOKEN is: ${envVariables.INVEST_TOKEN}`);
+//
+// import { createSdk } from 'tinkoff-sdk-grpc-js';
+//
+// !(async function example() {
+//     if (envVariables.INVEST_TOKEN) {
+//         const {users, operations} = createSdk(envVariables.INVEST_TOKEN);
+//
+//         const userInfo = await users.getInfo({});
+//         const accounts = await users.getAccounts({});
+//         const tarrif = await users.getUserTariff({});
+//         console.log('Информация о пользователе:', userInfo);
+//         console.log('Информация о счетах:', accounts);
+//         console.log('Информация о тарифе пользователя:', tarrif);
+//
+//         const positions = await operations.getPortfolio({accountId: '2054310628'})
+//         console.log("positions", positions)
+//
+//     }
+// })();
