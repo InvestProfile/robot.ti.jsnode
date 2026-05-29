f3d01ac39f4dbe52dbf5b2835450cf1d9c053b30
mil <milomory@gmail.com>
2026-05-29 11:10:36 +0300

    Cover protective stops without current price

--- app/modules/common.module.ts
+++ app/modules/common.module.ts
@@ -323,7 +323,6 @@ export const ensureProtectiveStopsForOpenRobotPositions = async (config: RobotCo
         const openItems = (ledger.items || []).filter(item =>
             Number(item.lots ?? 0) > 0
             && Number(item.averagePrice ?? 0) > 0
-            && Number(item.currentPrice ?? 0) > 0
             && item.accountId
             && item.figi
             && item.instrumentUid
